e20c9f6fae49843693bb5941b87df5caa3da379a
cnljli@live.com <cnljli@live.com>
2015-03-07 22:37:22 +0800

    top ok

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+(function(argument) {
+	window.requestAnimationFrame 
+	|| (window.requestAnimationFrame = window.webkitRequestAnimationFrame 
+		|| window.mozRequestAnimationFrame 
+		|| window.oRequestAnimationFrame 
+		|| window.msRequestAnimationFrame 
+		||function(callback, element) {
+		    	return window.setTimeout(function() {
+		       		return callback(+new Date());
+		    }, 1000 / 60)
+		});
+
+	function scrolstep() { 
+	 	var _y= scrollY;
+	    scrollTo(0,_y-15);
+	    if (_y > 0) {
+	    	requestAnimationFrame(scrolstep);
+	    }
+	}
+	(document.querySelector&&document.querySelector('a')||document.getElementsByClassName('back-top')[0]).onclick=scrolstep;
+})();
