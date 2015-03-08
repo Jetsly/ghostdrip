bc52b7504467f1d34b485454325e4af6c4584c00
Jetsly <cnljli@live.com>
2015-03-08 10:48:44 +0800

    add ad

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,13 +24,13 @@
 	actionlink('.site-bars').onclick=function(){
 		
 	};
-     document.write('<a style="display:none!important" id="tanx-a-mm_109076080_8994641_30312346"></a>');
+     document.write('<a style="display:none!important" id="tanx-a-mm_109076080_8994641_30312366"></a>');
      tanx_s = document.createElement("script");
      tanx_s.type = "text/javascript";
      tanx_s.charset = "gbk";
-     tanx_s.id = "tanx-s-mm_109076080_8994641_30312346";
+     tanx_s.id = "tanx-s-mm_109076080_8994641_30312366";
      tanx_s.async = true;
-     tanx_s.src = "http://p.tanx.com/ex?i=mm_109076080_8994641_30312346";
+     tanx_s.src = "http://p.tanx.com/ex?i=mm_109076080_8994641_30312366";
      tanx_h = document.getElementsByTagName("head")[0];
      if(tanx_h)tanx_h.insertBefore(tanx_s,tanx_h.firstChild);
 })();
